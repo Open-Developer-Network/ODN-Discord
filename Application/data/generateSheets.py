884e8580b0554e269044313864bf2944fc722993
Devell Robinson <devell.robinson@icloud.com>
2025-12-24 00:03:48 +0000

    chore: update generateSheets script to include new sheet identifiers and add empty JSON files for sheets 0-9 and _sym

--- Application/data/generateSheets.py
+++ Application/data/generateSheets.py
@@ -1,8 +1,8 @@
 import os
 
 
-sheets =["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
- 
+sheets =["_sym","0","1","2","3","4","5","6","7","8","9","A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 # Ensure folder exists
 folder = f"sheets"
 os.makedirs(folder, exist_ok=True)
@@ -15,4 +15,4 @@ for slot in sheets:
         userSheet.write("{}")
 
     print(f'Created/Placed .json for sheet {slot}')
- 
+print('All sheets created/placed successfully.')
